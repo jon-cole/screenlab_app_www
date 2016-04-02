cceb2d780d1c623a306108badad050222a610dfc
Jon Cole <jon@screenlab.com>
2016-04-02 16:05:06 +0100

    Adding basic user functions to login controller

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,17 @@
 angular.module('app.controllers', [])
   
-.controller('loginCtrl', function($scope) {
+.controller('loginCtrl', function(userService, $scope) {
+	$scope.email, $scope.password = userService.getUser();
+	
+	$scope.login = function() {
+			userService.verify($scope.email, $scope.password)
+			.then(function(){
+					userService.saveUser();
+				},function(){
+					
+				});
+			
+	};
 
 })
    
