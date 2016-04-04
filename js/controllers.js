38111cb2471fa802940a9ff6661b96ecd30b4ce3
Jonathan Cole <jon@screenlab.io>
2016-04-04 07:56:54 +0100

    Update controllers.js

--- js/controllers.js
+++ js/controllers.js
@@ -1,25 +1,28 @@
 angular.module('app.controllers', [])
-  
-.controller('loginCtrl', function(userService, $scope) {
-	$scope.email, $scope.password = userService.getUser();
-	
-	$scope.login = function() {
-			userService.verify($scope.email, $scope.password)
-			.then(function(){
-					userService.saveUser();
-				},function(){
-					
-				});
-			
-	};
+
+.controller('loginCtrl', function (userService, $scope) {
+    $scope.email, $scope.password = userService.getUser();
+
+    $scope.login = function () {
+        userService.verify($scope.email, $scope.password)
+            .then(function () {
+                userService.saveUser();
+                $state.transitionTo("login");
+            }, function () {
+                $ionicPopup.alert({
+                    title: 'Login Failure',
+                    content: 'Authentication failed. Please check your username and password and ensure you are connected to the internet.'
+                }).;
+            });
+
+    };
 
 })
-   
-.controller('yourScansCtrl', function($scope) {
+
+.controller('yourScansCtrl', function ($scope) {
 
 })
-   
-.controller('scanNameCtrl', function($scope) {
+
+.controller('scanNameCtrl', function ($scope) {
 
 })
- 
