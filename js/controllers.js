b57a69cd03223a090e19d6d9af5309a97c9da377
Jonathan Cole <jon@screenlab.io>
2016-04-05 08:14:07 +0100

    Update controllers.js

--- js/controllers.js
+++ js/controllers.js
@@ -1,161 +1,67 @@
-angular.module('app.services', [])
+angular.module('app.controllers', [])
 
-// tokenService:
-// This service takes your ScreenLab email and password to authenticate and
-// returns an access token. If it fails it will reject to be handled in the
-// calling controller.
-.factory('tokenService', function ($http, $q) {
-    var obj = {};
-    obj.getToken = function (email, password) {
-        return $q(function (resolve, reject) {
-            // This is the dev ScreenLab server.
-            var postUrl = 'http://162.243.110.9/api/auth/session';
-            var postObject = {};
-            postObject.email = email;
-            postObject.password = password;
-
-            $http({
-                url: postUrl,
-                method: "POST",
-                data: JSON.stringify(postObject)
-            }).then(function (response) {
-                response;
-                // Check if the response actually contains a token, in case the http post
-                // reported success without returning the correct JSON array.
-                if (!response || !response.data || !response.data.session || !response.data.session.accesstoken) {
-                    reject();
-                } else {
-                    resolve(response.data.session.accesstoken);
-                };
-            }, function (err) {
-                reject(err);
-            });
-
-
-        });
-    };
-    return obj;
-})
-
-
-// scanService:
-// This service handles sending and receiving ScreenLab tests
-// and results. 
-.factory('scanService', function ($http, $q, userService) {
-    var obj = {};
-    obj.postScan = function (token, imageData) {
-        return $q(function (resolve, reject) {
-
-            // This is the dev ScreenLab server.
-            var postUrl = "http://162.243.110.9/api/scan";
-
-            var timeDate = new Date().toLocaleString();
-            var postObject = new Object();
-            postObject.name = "App " + timeDate;
-            postObject.imageData = "data:image/jpeg;base64," + imageData;
-
-            $http({
-                url: postUrl,
-                method: "POST",
-                data: JSON.stringify(postObject),
-                headers: {
-                    'X-Accesstoken': token
-                }
-            }).then(function (res) {
-                if (!res.data || !res.data.s3_image_link) {
-                    reject();
-                } else {
-                    resolve(res.data);
-                }
-            });
-        });
-    };
-    obj.getScans = function (token) {
-        return $q(function (resolve, reject) {
-            var scansUrl = "http://162.243.110.9/api/scans";
-            $http({
-                url: scansUrl,
-                method: "GET",
-                headers: {
-                    'X-Accesstoken': token
-                }
-            }).then(function (res) {
-                if (!res.data) {
-                    userService.getUser().then(function (userDetails) {
-                        resolve(userDetails.scans);
-                    }, function (error) {
-                        reject();
-                    })
-                } else {
-                    userService.saveScans(res.data);
-                    resolve(res.data);
-                }
-            }, function (error) {
-                userService.getUser().then(function (userDetails) {
-                    resolve(userDetails.scans);
-                }, function (error) {
-                    reject();
+.controller('loginCtrl', function (userService, $scope, $ionicPopup, $state) {
+    var userDetails = userService.getUser();
+    $scope.email = userDetails.email;
+    $scope.password = userDetails.password;
+    $scope.login = function (email, password) {
+        userService.verify(email, password)
+            .then(function () {
+                userService.saveUser(email, password);
+                $ionicPopup.alert({
+                    title: 'Success!',
+                    content: 'Your details have been verified. Tap "OK" to go to your scan history.'
+                }).then(function () {
+                    $state.go('yourScans');
                 });
-            })
-        });
-    };
-    return obj;
-})
 
-
-
-// imageService:
-// This service gets a photo with the cordova camera plugin.
-// It could be replaced with various alternative image sources
-// that return a base 64 encoded image.
-.factory('imageService', function ($cordovaCamera) {
-    var obj = {};
-    obj.getImage = function () {
-        var options = {
-            quality: 75,
-            destinationType: Camera.DestinationType.DATA_URL,
-            sourceType: Camera.PictureSourceType.CAMERA,
-            allowEdit: 1,
-            encodingType: Camera.EncodingType.JPEG,
-            targetWidth: 1280,
-            targetHeight: 1280,
-            correctOrientation: 1,
-            saveToPhotoAlbum: false
-        };
-        return $cordovaCamera.getPicture(options);
-    };
-    return obj;
-})
-
-
-
-// userService: 
-// Allows verification of user details, saving user information for next
-// login and retrieving saved user information.
-.factory('userService', function (tokenService, $q) {
-    var obj = {};
-    obj.verify = function (email, password) {
-        return $q(function (resolve, reject) {
-            tokenService.getToken(email, password).then(function (response) {
-                resolve();
             }, function (error) {
-                reject(error);
+                $ionicPopup.alert({
+                    title: 'Login Failure',
+                    content: 'Authentication failed. Please check your username and password and ensure you are connected to the internet.'
+                });
             });
+
+    };
+
+})
+
+.controller('yourScansCtrl', function ($scope, userService, imageService, tokenService, scanService) {
+    var userDetails = userService.getUser();
+    var email = userDetails.email;
+    var password = userDetails.password;
+    $scope.scans = userDetails.scans;
+
+    $scope.getScans = function () {
+        tokenService.getToken(email, password).then(function (token) {
+            scanService.getScans(token).then(function (scans) {
+                $scope.scans = scans;
+            }, function (error) {
+                // Scan service error
+            })
+        }, function (error) {
+            // Token error
         });
     };
-    obj.getUser = function () {
-        return {
-            "email": window.localStorage.getItem("email"),
-            "password": window.localStorage.getItem("password"),
-            "scans": window.localStorage.getItem("scans")
-        };
+
+    $scope.newPhoto = function () {
+        tokenService.getToken(email, password).then(function (token) {
+            imageService.getImage().then(function (imageData) {
+                scanService.postScan(token, imageData).then(function (scanData) {
+                    $scope.img = scanData.s3_image_link;
+                }, function (error) {
+                    // No scan returned
+                });
+
+            }, function (error) {
+                // No image error
+            });
+        }, function (error) {
+            // Token failure
+        });
     };
-    obj.saveUser = function (email, password) {
-        window.localStorage.setItem("email", email);
-        window.localStorage.setItem("password", password);
-    };
-    obj.saveScans = function (scans) {
-        window.localStorage.setItem("scans", scans);
-    };
-    return obj;
+})
+
+.controller('scanNameCtrl', function ($scope) {
+
 })
