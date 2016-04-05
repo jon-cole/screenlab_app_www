ef68473dd81a96c6d4fce4032f2ce076bdd4c653
Jonathan Cole <jon@screenlab.io>
2016-04-05 16:11:44 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -13,5 +13,6 @@ angular.module('app', ['ionic', 'ngCordova', 'app.controllers', 'app.routes', 'a
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+    ImgCache.init();
   });
 })
