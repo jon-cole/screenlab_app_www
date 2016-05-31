f69bb4f463c4ec7288b91d20fc5c9cdfd89159fc
Jonathan Cole <jon@screenlab.io>
2016-05-31 22:38:21 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,7 +6,15 @@ angular.module('app', ['ionic', 'ngCordova', 'ionicLazyLoadCache', 'ngJustGage',
 //ImgCacheProvider.setOptions({ debug: true, usePersistentCache: true });
 //ImgCacheProvider.manualInit = true; })
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $rootScope, $state) {
+  $rootScope.on('$locationChangeStart', function(event, next, current) {
+    // check for the user's token and that we aren't going to the login view
+    var userDetails = userService.getUser();
+    if((angular.isUndefined(userDetails)||angular.isUndefined(userDetails.email)||userDetails.email=="") && next.templateUrl != 'login.html'){
+      // go to the login view
+      $state.go('login');
+    }
+  }
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
