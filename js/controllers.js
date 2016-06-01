8e94f57e3d249dc8eab4d4d0452724bf97c1558c
Jonathan Cole <jon@screenlab.io>
2016-06-01 15:07:28 +0100

    Update controllers.js

--- js/controllers.js
+++ js/controllers.js
@@ -119,7 +119,7 @@ angular.module('app.controllers', [])
         });
     $scope.showUrlPopup = function() {
         $scope.data = {}
-        var urlPopup = $ionicPopup.show({
+        $ionicPopup.show({
         template: 'templates/urlForm.html',
         title: 'Enter scan details',
         scope: $scope,
