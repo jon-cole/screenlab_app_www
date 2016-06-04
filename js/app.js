fc42d487c518e559267d043a6bcae4245d97a152
Jonathan Cole <jon@screenlab.io>
2016-06-04 14:41:07 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,23 +1,18 @@
 // ScreenLab App
-
 angular.module('app', ['ionic', 'ngCordova', 'ionicLazyLoadCache', 'ngJustGage', 'ngImgCache', 'app.controllers', 'app.routes', 'app.services'])
 
-//.config(function(ImgCacheProvider) {
-//ImgCacheProvider.setOptions({ debug: true, usePersistentCache: true });
-//ImgCacheProvider.manualInit = true; })
-
-.run(function($ionicPlatform, userService, $state) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-    
-    ImgCache.init();
-  });
+.run(function ($ionicPlatform, userService, $state) {
+    $ionicPlatform.ready(function () {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+        // Inititialise the image cache for offline use
+        ImgCache.init();
+    });
 })
