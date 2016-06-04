272e0b64b9701d5543a14aa72083c204d3be7dd5
Jonathan Cole <jon@screenlab.io>
2016-06-04 14:42:28 +0100

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,35 +1,33 @@
 angular.module('app.routes', [])
 
-.config(function($stateProvider, $urlRouterProvider) {
-
+.config(function ($stateProvider, $urlRouterProvider) {
+  // This app has three simple states:
+  // login for the user to login
+  // yourScans to show a list of scan results
+  // scanName to give a detailed view of a scan
+  
   $stateProvider
+    .state('login', {
+        url: '/login',
+        templateUrl: 'templates/login.html',
+        controller: 'loginCtrl'
+    })
 
+    .state('yourScans', {
+        url: '/scans',
+        templateUrl: 'templates/yourScans.html',
+        controller: 'yourScansCtrl'
+    })
 
-// This app has three simple states:
-// login for the user to login
-// yourScans to show a list of scan results
-// scanName to give a detailed view of a scan
-  .state('login', {
-    url: '/login',
-    templateUrl: 'templates/login.html',
-    controller: 'loginCtrl'
-  })
+    .state('scanName', {
+        url: '/scan',
+        templateUrl: 'templates/scanName.html',
+        controller: 'scanNameCtrl',
+        params: {
+            scan: null
+        }
+    })
 
-  .state('yourScans', {
-    url: '/scans',
-    templateUrl: 'templates/yourScans.html',
-    controller: 'yourScansCtrl'
-  })
-
-  .state('scanName', {
-    url: '/scan',
-    templateUrl: 'templates/scanName.html',
-    controller: 'scanNameCtrl',
-    params: {scan : null }
-  })
-
-$urlRouterProvider.otherwise('/login')
-
-
+    $urlRouterProvider.otherwise('/login')
 
 });
